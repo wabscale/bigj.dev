0934a17f7d3153e2de96aa975fe721b706dc0d8c
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2019-05-21 19:20:17 -0400

    implement otp

--- src/routes/files.js
+++ src/routes/files.js
@@ -1,6 +1,6 @@
 const Router = require('koa-router');
 const send = require('koa-send');
-const {getFileByFilename, addDownload} = require('../../db');
+const {getFileByFilename, addDownload, verifyOtp} = require('../../db');
 const {UPLOAD_PATH} = require('../../config');
 
 const router = new Router({
@@ -9,12 +9,16 @@ const router = new Router({
 
 router.get('/:requestedFilename', async (ctx, next) => {
   const {user} = ctx.state;
+  const {otp} = ctx.request.query;
   const ipAddress = ctx.request.header['X-Forwarded-For'] || ctx.request.ip;
   const {requestedFilename} = ctx.params;
   const file = await getFileByFilename(requestedFilename);
-  if ((!file) || (!file.isPublic && !user))
-    return addDownload(file.id, ipAddress, false);
-  addDownload(file.id, ipAddress, true);
+  if (!file)
+    return addDownload(0, ipAddress, false);
+  const {id} = file;
+  if ((!file.isPublic && !user) || await verifyOtp(otp))
+    return addDownload(id, ipAddress, false);
+  addDownload(id, ipAddress, true);
   await send(ctx, requestedFilename, { root: UPLOAD_PATH });
   await next();
 });
