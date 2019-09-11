d459b143e1786ea02a96112200ddaefe11d25ea8
big_J <johncunniff1248@gmail.com>
2019-09-11 03:28:06 +0000

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -6,18 +6,6 @@ set -e
 
 export UPLOAD_PATH=/home/jc/uploads
 
-if ! which mysqladmin; then
-  sudo apt update && sudo apt install -y mysql-client
-fi
-
-if ! which node; then
-  sudo apt update && sudo apt install -y nodejs npm
-fi
-
-if ! which sequelize; then
-  sudo npm install -g sequelize sequelize-cli mysql2
-fi
-
 if [ ! -f /etc/traefik/acme/acme.json ]; then
     sudo mkdir -p /etc/traefik/acme/
     sudo touch /etc/traefik/acme/acme.json
@@ -32,14 +20,3 @@ docker-compose build # --no-cache
 docker-compose kill
 docker-compose rm -f
 docker-compose up -d --force-recreate db api frontend traefik
-
-echo "waiting for db to start..."
-while ! mysqladmin ping -h "127.0.0.1" -P 3306 --silent; do
-  sleep 1;
-done
-sleep 3
-echo "db started"
-
-cd db
-
-sequelize db:migrate
