53a549b40f9afa5b4fe5aaf158223bfd001d2470
big_J <johncunniff1248@gmail.com>
2019-09-14 20:00:16 -0400

    another fix

--- api/src/auth.js
+++ api/src/auth.js
@@ -37,7 +37,7 @@ loadUser = async (ctx, next) => {
     const loadedToken = ctx.req.headers.token || cookies.token || ctx.params.token;
     const token = jwt.verify(loadedToken, await loadKey());
     ctx.state.user = await getUserById(token.id);
-    ctx.set('Set-Cookie', 'token=' + loadedToken);
+    ctx.set('Set-Cookie', 'token=' + loadedToken + '; Domain=api.bigj.dev');
   } catch(err) {}
   await next();
 };
