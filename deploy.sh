7dd2ac07b2457d3e4fbdba3f1b5862fc5448477d
big_J <johncunniff1248@gmail.com>
2019-11-15 01:15:19 +0000

    deploy stuff

--- deploy.sh
+++ deploy.sh
@@ -6,29 +6,26 @@ set -e
 
 export UPLOAD_PATH=/home/jc/uploads
 
-if [ ! -f /etc/traefik/acme/acme.json ]; then
-    sudo mkdir -p /etc/traefik/acme/
-    sudo touch /etc/traefik/acme/acme.json
-    sudo chmod 600 /etc/traefik/acme/acme.json
-fi
+# if [ ! -f /etc/traefik/acme/acme.json ]; then
+#     sudo mkdir -p /etc/traefik/acme/
+#     sudo touch /etc/traefik/acme/acme.json
+#     sudo chmod 600 /etc/traefik/acme/acme.json
+# fi
 
 if ! docker network ls | grep "traefik-proxy"; then
   docker network create traefik-proxy
 fi
 
-docker-compose build # --no-cache
-docker-compose kill
-docker-compose rm -f
-docker-compose up -d --force-recreate api frontend
+docker-compose up -d --force-recreate --build api frontend redirect
 
-if ! docker ps | grep 'traefik' | grep 'Up'; then
-    docker-compose up -d --force-recreate traefik
-fi
+# if ! docker ps | grep 'traefik' | grep 'Up'; then
+#     docker-compose up -d --force-recreate traefik
+# fi
 
-if ! docker ps | grep 'redirect' | grep 'Up'; then
-    docker-compose up -d --force-recreate redirect
-fi
+# if ! docker ps | grep 'redirect' | grep 'Up'; then
+#     docker-compose up -d --force-recreate redirect
+# fi
 
-if ! docker ps | grep 'mariadb' | grep 'Up'; then
-    docker-compose up -d --force-recreate db
-fi
+# if ! docker ps | grep 'mariadb' | grep 'Up'; then
+#     docker-compose up -d --force-recreate db
+# fi
